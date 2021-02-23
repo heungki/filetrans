fad761e9ebb20072937cdb5751bd5d0e7ca1276a
kimhh <kimhh@DESKTOP-R1TA287>
2021-02-23 17:19:11 +0900

    filetransdaemon init commit

---
+++ filetransdaemon/src/main/java/com/filetransdaemon/FiletransdaemonApplication.java
@@ -0,0 +1,13 @@
+package com.filetransdaemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransdaemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransdaemonApplication.class, args);
+	}
+
+}
---
+++ filetransdaemon/src/test/java/com/filetransdaemon/FiletransdaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filetransdaemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiletransdaemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
