096b8ae214e5e965a36e366287a1bc0ce32ae447
kimhh <kimhh@DESKTOP-R1TA287>
2021-02-23 16:59:05 +0900

    client init commit

---
+++ filetransclient/src/main/java/com/filetransclient/FiletransclientApplication.java
@@ -0,0 +1,13 @@
+package com.filetransclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiletransclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiletransclientApplication.class, args);
+	}
+
+}
---
+++ filetransclient/src/test/java/com/filetransclient/FiletransclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filetransclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiletransclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
